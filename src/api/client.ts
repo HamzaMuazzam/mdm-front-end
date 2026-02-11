eaea68cc3484ce027521176fef54b53daddc9055
Hamza Muazzam <hamzamuazzam@gmail.com>
2026-02-11 12:27:22 +0500

    ip change

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'http://10.10.10.81:9000';
-const API_BASE_URL = 'http://192.168.0.211:9000';
+const API_BASE_URL = 'http://10.10.10.81:9000';
+// const API_BASE_URL = 'http://192.168.0.211:9000';
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
