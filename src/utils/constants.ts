d5bcbd58986dce0249b8b8ae0c9259bdfbf228cc
Hamza Muazzam <hamzamuazzam@gmail.com>
2026-02-25 10:45:01 +0500

    ws added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 
 //TODO: for live server only
-export const WS = 'ws2';
-export const API_BASE_URL = 'http://10.10.10.32:9000';
-export const MQTT_BROKER_URL = `${WS}://10.10.10.32:8084/mqtt`;
+export const WS = 'wss';
+export const API_BASE_URL = 'https://mdm.dspl.pk/api';
+export const MQTT_BROKER_URL = `${WS}://mdm.dspl.pk:8084/mqtt`;
 
 
 //TODO: for local development
