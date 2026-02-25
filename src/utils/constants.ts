b2be93f4dedfe28caf7b2a29da64dc2714e75624
Hamza Muazzam <hamzamuazzam@gmail.com>
2026-02-25 10:38:51 +0500

    analytics added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 
 //TODO: for live server only
-export const WS = 'wss';
-export const API_BASE_URL = 'https://10.10.10.32';
+export const WS = 'ws';
+export const API_BASE_URL = 'http://10.10.10.32';
 export const MQTT_BROKER_URL = `${WS}://10.10.10.32:8084/mqtt`;
 
 
 //TODO: for local development
 // export const WS = 'ws';
-// export const API_BASE_URL = 'https://10.10.10.81';
+// export const API_BASE_URL = 'http://10.10.10.81';
 // export const MQTT_BROKER_URL = `${WS}://10.10.10.81:8083/mqtt`;
 
 
